46d4fd97c28173585e66d410726495ab64705094
phoenix986 <phoenix986@gmail.com>
2020-03-14 20:55:48 +0100

    added beacon monitoring

--- CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/MainActivity.java
+++ CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.coronavirusherdimmunity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        findViewById(R.id.openMonitoring).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(mContext, MonitoringActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
