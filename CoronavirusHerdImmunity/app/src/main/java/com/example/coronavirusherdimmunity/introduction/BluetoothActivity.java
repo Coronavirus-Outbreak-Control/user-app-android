790c513377d7fd56c56e20dc44cbab42bc5471d0
FrancescoBonarota <francesco.bonarota@gmail.com>
2020-03-16 01:43:36 +0100

    Introduced permission management
    Introduced permission management:
    - Require and enable Bluetooth (works)
    - Require and enable Location (not works very well)
    - Require and enable Notification (not works very well)

--- CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/introduction/BluetoothActivity.java
+++ CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/introduction/BluetoothActivity.java
@@ -1,6 +1,9 @@
 package com.example.coronavirusherdimmunity.introduction;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,8 +16,13 @@ import com.example.coronavirusherdimmunity.HowItWorksActivity;
 import com.example.coronavirusherdimmunity.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
 
 public class BluetoothActivity extends AppCompatActivity {
+
+    private final int REQUEST_ID_PERMISSIONS_BLUETOOTH = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +36,13 @@ public class BluetoothActivity extends AppCompatActivity {
 
         Button button_next, button_skip;
 
-
         button_next = findViewById(R.id.button_next);
         button_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(BluetoothActivity.this, LocationActivity.class));
-                finish();
+
+                requestBTPermission(); //requires bluetooth permissions and turn on
+
             }
         });
 
@@ -48,4 +56,41 @@ public class BluetoothActivity extends AppCompatActivity {
         });
 
     }
+
+    /**
+     * Require Bluetooth permission, turn on it, go to next activity
+     */
+    private void requestBTPermission(){
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+        if (!bluetoothAdapter.isEnabled()) { // if bluetooth is not enabled then turn on
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ID_PERMISSIONS_BLUETOOTH);
+        } else { //else bluetooth is enabled then go to next activity
+            startActivity(new Intent(BluetoothActivity.this, LocationActivity.class));
+            finish();
+        }
+
+    }
+
+
+    /**
+     * When the user responds to your app's permission request, the system invokes this function.
+     * This function check if the permissions are granted or not, then go to next activity
+     */
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ID_PERMISSIONS_BLUETOOTH: {
+                startActivity(new Intent(BluetoothActivity.this, LocationActivity.class));
+                finish();
+            }
+        }
+    }
+
 }
+
--- CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/introduction/NotificationsActivity.java
+++ CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/introduction/NotificationsActivity.java
@@ -1,6 +1,8 @@
 package com.example.coronavirusherdimmunity.introduction;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -12,8 +14,12 @@ import com.example.coronavirusherdimmunity.MainActivity;
 import com.example.coronavirusherdimmunity.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class NotificationsActivity extends AppCompatActivity {
+
+    private final int REQUEST_ID_PERMISSION_NOTIFICATION = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +38,10 @@ public class NotificationsActivity extends AppCompatActivity {
         button_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(NotificationsActivity.this, MainActivity.class));
-                finish();
+
+                requestNotificationPermission();
+                //startActivity(new Intent(NotificationsActivity.this, MainActivity.class));
+                //finish();
             }
         });
 
@@ -46,5 +54,47 @@ public class NotificationsActivity extends AppCompatActivity {
             }
         });
 
+    }
+    /**
+     * Require and enable Notification permission, go to next activity
+     */
+    private void requestNotificationPermission() {
+
+        //if location permission is not granted then request permission
+        if (ActivityCompat.checkSelfPermission(NotificationsActivity.this, Manifest.permission.ACCESS_NOTIFICATION_POLICY) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(NotificationsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_NOTIFICATION_POLICY},
+                    REQUEST_ID_PERMISSION_NOTIFICATION);
+        }
+
+        NotificationManagerCompat.getEnabledListenerPackages(getApplicationContext()).contains(getApplicationContext().getPackageName());
+
+        startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS").addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+
+        if (!NotificationManagerCompat.getEnabledListenerPackages (getApplicationContext()).contains(getApplicationContext().getPackageName())) {
+            //service is not enabled try to enabled by calling
+            getApplicationContext().startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS").addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        } else {
+            //Your own logic
+        }
+    }
+
+
+    /**
+     * When the user responds to your app's permission request, the system invokes this function.
+     * This function check if the permissions are granted or not, then go to next activity
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_ID_PERMISSION_NOTIFICATION: {
+                startActivity(new Intent(NotificationsActivity.this, MainActivity.class));
+                finish();
+            }
+        }
     }
 }
