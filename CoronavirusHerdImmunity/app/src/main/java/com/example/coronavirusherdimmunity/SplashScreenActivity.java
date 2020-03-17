02f47670e0871bc15ba03c9ea540d3ea7b1bc89f
Elena Martina <elenamartina98@gmail.com>
2020-03-17 21:32:19 +0000

    Merge Fix

--- CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/SplashScreenActivity.java
+++ CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/SplashScreenActivity.java
@@ -1,8 +1,10 @@
 package com.example.coronavirusherdimmunity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -13,13 +15,11 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SplashScreenActivity extends AppCompatActivity {
 
     private PreferenceManager prefManager;
-    Handler handler;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //Remove notification bar
@@ -28,12 +28,8 @@ public class SplashScreenActivity extends AppCompatActivity {
         //set content view AFTER ABOVE sequence (to avoid crash)
         this.setContentView(R.layout.splashscreen);
 
-        handler=new Handler();
-
         // Checking for first time launch - before calling setContentView()
         prefManager = new PreferenceManager(this);
-
-
         if (!prefManager.isFirstTimeLaunch()) {
 
             launchHomeScreen();
@@ -62,30 +58,9 @@ public class SplashScreenActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
                 finish();
             }
         },3000);
     }
-    private void launchIntroduction() {
-
-        this.prefManager.setFirstTimeLaunch(false);
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, WelcomeActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },2000);
-
-    }
-
-    @Override
-    public void onBackPressed(){
-        finish();
-    }
-
 }
