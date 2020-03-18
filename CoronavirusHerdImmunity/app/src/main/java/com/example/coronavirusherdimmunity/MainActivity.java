2677009c644741540b6e9da89cbd63514e6604f8
Elena Martina <elenamartina98@gmail.com>
2020-03-18 22:01:00 +0000

    QR Code generator class

--- CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/MainActivity.java
+++ CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/MainActivity.java
@@ -1,31 +1,19 @@
 package com.example.coronavirusherdimmunity;
 
-import androidmads.library.qrgenearator.QRGContents;
-import androidmads.library.qrgenearator.QRGEncoder;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Bitmap;
-import android.graphics.Point;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.ImageView;
 
-import com.google.zxing.WriterException;
+import com.example.coronavirusherdimmunity.utils.QRCodeGenerator;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Context mContext;
-    String TAG = "GenerateQRCode";
-    ImageView qrImage;
-    String inputValue;
-    Bitmap bitmap;
-    QRGEncoder qrgEncoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         mContext = this;
 
-        qrImage = (ImageView) findViewById(R.id.qr_code);
-        int deviceId = new PreferenceManager(getApplicationContext()).getDeviceId();
-        inputValue = Integer.toString(deviceId);
-        /*start = (Button) findViewById(R.id.start);*/
-        /*save = (Button) findViewById(R.id.save);*/
+        // QR Code stuff
+        QRCodeGenerator gen = new QRCodeGenerator(mContext);
+        int deviceId = new PreferenceManager(mContext.getApplicationContext()).getDeviceId();
+        ImageView qrImage = (ImageView) findViewById(R.id.qr_code);
+
+        gen.generateQRCode(deviceId, qrImage);
 
 
         findViewById(R.id.openMonitoring).setOnClickListener(new View.OnClickListener() {
@@ -49,27 +38,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        Display display = manager.getDefaultDisplay();
-
-        Point point = new Point();
-        display.getSize(point);
-        int width = point.x;
-        int height = point.y;
-        int smallerDimension = width < height ? width : height;
-        smallerDimension = smallerDimension / 2;
-
-        qrgEncoder = new QRGEncoder(
-                inputValue, null,
-                QRGContents.Type.TEXT,
-                smallerDimension);
-        try {
-            bitmap = qrgEncoder.encodeAsBitmap();
-            qrImage.setImageBitmap(bitmap);
-        } catch (WriterException e) {
-            Log.v(TAG, e.toString());
-        }
-
     }
+
 }
---
+++ CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/utils/QRCodeGenerator.java
@@ -0,0 +1,52 @@
+package com.example.coronavirusherdimmunity.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import com.google.zxing.WriterException;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+import static android.content.Context.WINDOW_SERVICE;
+
+
+public class QRCodeGenerator {
+
+    private Context mContext;
+    private String TAG = "GenerateQRCode";
+    private String inputValue;
+    private Bitmap bitmap;
+    private QRGEncoder qrgEncoder;
+
+    public QRCodeGenerator(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public void generateQRCode(int deviceId, ImageView qrImage) {
+
+        inputValue = Integer.toString(deviceId);
+
+        WindowManager manager = (WindowManager) mContext.getSystemService(WINDOW_SERVICE);
+        Display display = manager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+
+        int width = point.x;
+        int height = point.y;
+        int dimension = (Math.min(width, height)) / 2;
+
+
+        qrgEncoder = new QRGEncoder(inputValue, null, QRGContents.Type.TEXT, dimension);
+
+        try {
+            bitmap = qrgEncoder.encodeAsBitmap();
+            qrImage.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            Log.v(TAG, e.toString());
+        }
+    }
+
+}
