29d80cd81d56de522c06746d989248e98ba32d6d
Elena Martina <elenamartina98@gmail.com>
2020-03-18 22:07:10 +0000

    Small design fix x2

--- CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/utils/QRCodeGenerator.java
+++ CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/utils/QRCodeGenerator.java
@@ -36,7 +36,7 @@ public class QRCodeGenerator {
 
         int width = point.x;
         int height = point.y;
-        int dimension = (Math.min(width, height)) * 3 / 2;
+        int dimension = (Math.min(width, height)) * 2 / 3;
 
 
         qrgEncoder = new QRGEncoder(inputValue, null, QRGContents.Type.TEXT, dimension);
