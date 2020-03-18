e421954f7b394c5cac995320025152f8abe285c5
Elena Martina <elenamartina98@gmail.com>
2020-03-18 21:19:43 +0000

    Generated QR code

--- CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/MainActivity.java
+++ CoronavirusHerdImmunity/app/src/main/java/com/example/coronavirusherdimmunity/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.coronavirusherdimmunity;
 
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Typeface;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.google.zxing.WriterException;
 
 public class MainActivity extends AppCompatActivity {
 
     private Context mContext;
+    String TAG = "GenerateQRCode";
+    ImageView qrImage;
+    String inputValue;
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
         mContext = this;
 
+        qrImage = (ImageView) findViewById(R.id.qr_code);
+        int deviceId = new PreferenceManager(getApplicationContext()).getDeviceId();
+        inputValue = Integer.toString(deviceId);
+        /*start = (Button) findViewById(R.id.start);*/
+        /*save = (Button) findViewById(R.id.save);*/
+
+
         findViewById(R.id.openMonitoring).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,5 +48,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+
+
+        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        Display display = manager.getDefaultDisplay();
+
+        Point point = new Point();
+        display.getSize(point);
+        int width = point.x;
+        int height = point.y;
+        int smallerDimension = width < height ? width : height;
+        smallerDimension = smallerDimension / 2;
+
+        qrgEncoder = new QRGEncoder(
+                inputValue, null,
+                QRGContents.Type.TEXT,
+                smallerDimension);
+        try {
+            bitmap = qrgEncoder.encodeAsBitmap();
+            qrImage.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            Log.v(TAG, e.toString());
+        }
+
     }
 }
